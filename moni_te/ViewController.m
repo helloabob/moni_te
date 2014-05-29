4d5360788e1f5a4f4528faa072ab4cb0f14697bc
helloabob <wangbo@wangbos-iMac.local>
2014-05-29 21:44:45 +0800

    Initial Commit

---
+++ moni_te/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  moni_te
+//
+//  Created by wangbo on 5/29/14.
+//  Copyright (c) 2014 wb. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
